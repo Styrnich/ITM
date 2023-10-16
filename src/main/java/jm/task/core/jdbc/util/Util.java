38d4f9d5feac71b698324f699c74e4b93bab1073
Anastasia Shelkova <9053939@mail.ru>
2023-10-16 23:48:25 +0700

    done

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,38 @@
 package jm.task.core.jdbc.util;
 
+
+import jm.task.core.jdbc.model.User;
+import net.bytebuddy.implementation.bytecode.Throw;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+   private static SessionFactory factory;
+    public Util() {
+    }
+    public static SessionFactory getCon(){
+        try {
+            factory = new Configuration()
+                    .addAnnotatedClass(User.class)
+                    .buildSessionFactory();
+        }catch (Throwable e){
+            e.printStackTrace();
+        }
+
+        return factory;
+
+    }
+    public static void closeConnection() {
+        if (factory != null)
+            factory.close();
+    }
+
 }
+
